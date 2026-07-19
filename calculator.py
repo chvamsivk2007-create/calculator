e02e741d0b33ed4e07d0db048c2038b57e3a44e2
Chanumolu Vamsi Krishna <chvamsivk2007@gmail.com>
2026-07-19 18:05:44 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,49 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+def calculator():
+    operations = {"+":add,"-":subtract,"*":multiply,"/":divide}
+    print(logo)
+    num1 = int(input("Enter the first number: "))
+    should_accumulate = True
+    while should_accumulate:
+        for key in operations:
+            print(key)
+        operation_symbol = input("Enter the operation: ")
+        num2 = int(input("Enter the second number: "))
+        if operation_symbol in operations:
+            result = operations[operation_symbol](num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {result}")
+            choice = input(f"Type 'y' to continue calculating with {result} or type 'n' to stop : ")
+            if choice == "y":
+                num1 = result
+            else:
+                should_accumulate = False
+                print("\n" * 20)
+                calculator()
+calculator()
